5bf8eedc20b6ffb164756feb4e1a0ee2af16094c
Erin Dawson <erindawson.work@gmail.com>
2020-04-28 09:31:02 -0700

    Added API content

--- scripts.js
+++ scripts.js
@@ -9,54 +9,54 @@ const docSamplesDescription = document.getElementById("docSamplesDescription");
 const resumeDescription = document.getElementById("resumeDescription");
 const image = document.getElementById("actualImage");
 
-portfolioItem1.addEventListener('mouseover', function () {
-    // $("#gitArticleDescription").fadeIn("slow");
-    navProjectDescription.style.display = "none";
-    gitArticleDescription.style.display = "block";
-    image.src = "githubArticleNoBackground.gif"
-    docSamplesDescription.style.display = "none";
-    resumeDescription.style.display = "none";
-    image.style.visibility = "visible";
-    gitArticleDescription.style.visibility = "visible";
-    document.getElementById("itemDescription").style.borderRightColor = "black";
-})
+portfolioItem1.addEventListener("mouseover", function() {
+  // $("#gitArticleDescription").fadeIn("slow");
+  navProjectDescription.style.display = "none";
+  gitArticleDescription.style.display = "block";
+  image.src = "githubArticleNoBackground.gif";
+  docSamplesDescription.style.display = "none";
+  resumeDescription.style.display = "none";
+  image.style.visibility = "visible";
+  gitArticleDescription.style.visibility = "visible";
+  document.getElementById("itemDescription").style.borderRightColor = "black";
+});
 
 //If Dynamic navigation recieves mouseover
-portfolioItem2.addEventListener('mouseover', function () {
-    // $("#navProjectDescription").fadeIn("slow");
-    gitArticleDescription.style.display = "none";
-    gitArticleDescription.style.visibility = "hidden";
-    docSamplesDescription.style.display = "none";
-    docSamplesDescription.style.visibility = "hidden";
-    resumeDescription.style.display = "none";
-    navProjectDescription.style.display = "block";
-    image.style.visibility = "visible";
-    image.src = "dynamicWebsiteNoBackground.gif";
-    document.getElementById("itemDescription").style.borderRightColor = "black";
-})
+portfolioItem2.addEventListener("mouseover", function() {
+  // $("#navProjectDescription").fadeIn("slow");
+  gitArticleDescription.style.display = "none";
+  gitArticleDescription.style.visibility = "hidden";
+  docSamplesDescription.style.display = "none";
+  docSamplesDescription.style.visibility = "hidden";
+  resumeDescription.style.display = "none";
+  navProjectDescription.style.display = "block";
+  image.style.visibility = "visible";
+  image.src = "dynamicWebsiteNoBackground.gif";
+  document.getElementById("itemDescription").style.borderRightColor = "black";
+});
 
 //If docSamplesDescription recieves a mouseover
-portfolioItem3.addEventListener('mouseover', function () {
-    // $("#docSamplesDescription").fadeIn("slow");
-    docSamplesDescription.style.display = "block";
-    docSamplesDescription.style.visibility = "visible";
-    gitArticleDescription.style.display = "none";
-    gitArticleDescription.style.visibility = "hidden";
-    navProjectDescription.style.display = "none";
-    resumeDescription.style.display = "none";
-    image.src = "figure8_loRes.png";
-    document.getElementById("itemDescription").style.borderRightColor = "black";
-})
+portfolioItem3.addEventListener("mouseover", function() {
+  // $("#docSamplesDescription").fadeIn("slow");
+  docSamplesDescription.style.display = "block";
+  docSamplesDescription.style.visibility = "visible";
+  gitArticleDescription.style.display = "none";
+  gitArticleDescription.style.visibility = "hidden";
+  navProjectDescription.style.display = "none";
+  resumeDescription.style.display = "none";
+  image.src = "figure8_loRes.png";
+  document.getElementById("itemDescription").style.borderRightColor = "black";
+});
 
-portfolioItem4.addEventListener('mouseover', function(){
-    // $("#resumeDescription").fadeIn("slow");
-    resumeDescription.style.display = "block";
-    gitArticleDescription.style.display = "none";
-    navProjectDescription.style.display = "none";
-    docSamplesDescription.style.display = "none";
-    image.style.visibility = "hidden";
-    document.getElementById("itemDescription").style.borderRightColor = "transparent";
-    gitArticleDescription.style.visibility = "hidden";
-    navProjectDescription.style.display = "none";
-
-})
+portfolioItem4.addEventListener("mouseover", function() {
+  // $("#resumeDescription").fadeIn("slow");
+  resumeDescription.style.display = "block";
+  gitArticleDescription.style.display = "none";
+  navProjectDescription.style.display = "none";
+  docSamplesDescription.style.display = "none";
+  image.style.visibility = "visible";
+  image.src = "wordnik-api.gif";
+  // document.getElementById("itemDescription").style.borderRightColor = "transparent";
+  gitArticleDescription.style.visibility = "hidden";
+  navProjectDescription.style.display = "none";
+});
